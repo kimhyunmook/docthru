cedf113108c62b52d2ee6bd57c001c7a3d76ca5d
kimhyunmook_mac <gusanr4200@naver.com>
2025-01-22 13:16:14 +0900

    app____ del

--- client/app__/shared/types/common.tsx
+++
@@ -1,5 +0,0 @@
-import { PropsWithChildren } from "react";
-
-export interface PropsWithClassName<T = {}> extends PropsWithChildren {
-  className?: string;
-}
--- client/app__/shared/types/user.tsx
+++
@@ -1,6 +0,0 @@
-export interface User {
-  id?: string;
-  name?: string;
-  grade?: "일반" | "어드민";
-  heart?: number;
-}
