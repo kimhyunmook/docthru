e5352de0b3f2dc7051408ae855fdd05a2e383086
kimhyunmook_mac <gusanr4200@naver.com>
2025-02-19 14:52:52 +0900

    a

--- client/app/shared/provider/authProvider.tsx
+++ client/app/shared/provider/authProvider.tsx
@@ -41,6 +41,7 @@ export function AuthProvider({ children }: PropsWithChildren) {
     data: user,
     isStale,
     isFetching,
+    isFetched,
     isLoading,
     refetch: userRefetch,
   } = useQuery({
@@ -100,21 +101,22 @@ export function AuthProvider({ children }: PropsWithChildren) {
     }
   }
 
-  return (
-    <AuthContext.Provider
-      value={{
-        login,
-        refreshToken,
-        isLoading,
-        isFetching,
-        logout,
-        user,
-        auth,
-      }}
-    >
-      {children}
-    </AuthContext.Provider>
-  );
+  if (isFetched)
+    return (
+      <AuthContext.Provider
+        value={{
+          login,
+          refreshToken,
+          isLoading,
+          isFetching,
+          logout,
+          user,
+          auth,
+        }}
+      >
+        {children}
+      </AuthContext.Provider>
+    );
 }
 
 export function useAuth() {
