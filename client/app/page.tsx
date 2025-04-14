f8e017578c66a7d18580888bee6bf32fb9303c07
kimhyunmook_mac <gusanr4200@naver.com>
2025-04-14 16:13:59 +0900

    .

--- client/app/page.tsx
+++ client/app/page.tsx
@@ -1,8 +1,10 @@
 "use client";
 import Logo from "./shared/components/logo";
 import s from "./page.module.css";
+import { useEffect } from "react";
 
 export default function Home({}) {
+  useEffect(() => {}, []);
   return (
     <div className={s.page}>
       <Logo
